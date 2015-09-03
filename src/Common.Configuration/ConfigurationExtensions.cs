494a4aee3547aede49eee425471e4805b6cb91f4
jakub.pawlowski <jakub.pawlowski@legimi.com>
2015-09-03 08:23:20 +0200

    config: convert Iconfiguration to Dictionary

--- src/Common.Configuration/ConfigurationExtensions.cs
+++ src/Common.Configuration/ConfigurationExtensions.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Common.Configuration;
+using Microsoft.Framework.ConfigurationModel.Internal;
 
 namespace Microsoft.Framework.ConfigurationModel
 {
@@ -76,6 +77,84 @@ namespace Microsoft.Framework.ConfigurationModel
             var v = (cfg.Get(key));
             if (v == null) return default(T);
             else return cfg.Get<T>(key);
+        }
+
+        public static void Traverse(this IConfiguration configuration, Action<string, string> action, string rootNs = "")
+        {
+            var keys = configuration.GetSubKeys();
+
+            string val = null;
+            if (configuration is ConfigurationFocus)
+            {
+                val = ((ConfigurationFocus)configuration).Get(null);
+            }
+            if (keys != null)
+            {
+                foreach (var key in keys)
+                {
+                    Traverse(key.Value, action, rootNs + ":" + key.Key);
+                }
+            }
+            if (val != null)
+            {
+                var key = rootNs.TrimStart(':');
+                action(key, val);
+            }
+        }
+
+        public static TResult Aggregate<TResult>(this IConfiguration configuration,
+            TResult seed,
+            Func<TResult, string, object, TResult> action, string rootNs = "")
+        {
+            return Aggregate(configuration, action, rootNs);
+        }
+        private static TResult Aggregate<TResult>(this IConfiguration configuration, Func<TResult, string, object, TResult> action, string rootNs = "")
+        {
+            var keys = configuration.GetSubKeys();
+
+            string val = null;
+            if (configuration is ConfigurationFocus)
+            {
+                val = ((ConfigurationFocus)configuration).Get(null);
+            }
+            TResult ag = default(TResult);
+            if (keys != null)
+            {
+                foreach (var key in keys)
+                {
+                    var subag = Aggregate(key.Value, action, rootNs + ":" + key.Key);
+                    ag = action(ag, rootNs + ":" + key.Key, subag);
+                }
+            }
+            if (val != null)
+            {
+                var key = rootNs.TrimStart(':');
+                ag = action(ag, key, val);
+            }
+
+            return ag;
+        }
+        public static Dictionary<string, string> AsDictionaryPlain(this IConfiguration config)
+        {
+            var dict = new Dictionary<string, string>();
+            config.Traverse((s, s1) =>
+            {
+                dict.Add(s,s1);
+            });
+
+            return dict;
+        }
+
+        public static Dictionary<string, object> AsDictionaryNested(this IConfiguration config)
+        {
+            return config.Aggregate(new Dictionary<string, object>(),
+                (dict, key, val) =>
+                {
+                    if (dict == null) dict = new Dictionary<string, object>();
+                    key = key.Split(':').Last();
+                    dict.Add(key, val);
+                    return dict;
+                });
         }
     }
 }
