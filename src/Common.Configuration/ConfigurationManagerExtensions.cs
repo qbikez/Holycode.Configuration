b674456735f1d151a681339ce25a3b8fe9701b19
jakub.pawlowski <jakub.pawlowski@legimi.com>
2015-11-26 11:29:11 +0100

    common.configuration: fix net451 build

--- src/Common.Configuration/ConfigurationManagerExtensions.cs
+++ src/Common.Configuration/ConfigurationManagerExtensions.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿#if !DNX451
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using Microsoft.Framework.Configuration;
+using System.Configuration;
 
 namespace System.Configuration
 {
@@ -38,4 +41,28 @@ namespace System.Configuration
         }
     }
 
+    public static partial class ConnectionStringsExtenisons
+    {
+        /// <summary>
+        /// extracts connections strings from config.json into ConnectionStringSettingsCollection (eg. ConfigurationManager.ConnectionStrings)
+        /// </summary>
+        /// <param name="cfg"></param>
+        /// <param name="connectionStrings"> (eg. ConfigurationManager.ConnectionStrings)</param>
+        public static void ExtractConnectionStrings(this IConfiguration cfg,
+            ConnectionStringSettingsCollection connectionStrings)
+        {
+            var section = cfg.GetSection("connectionStrings");
+            var subKeys = section.GetChildren();
+
+            foreach (var subkey in subKeys)
+            {
+                var name = subkey.Key;
+                var val = subkey.Value ?? subkey.Get("connectionString");
+                var provider = subkey.Get("provider") ?? "System.Data.SqlClient";
+                connectionStrings.AddConnectionString(name, val, provider);
+            }
+        }
+    }
+
 }
+#endif
--- src/Common.Configuration/ConnectionStringsExtenisons.cs
+++ src/Common.Configuration/ConnectionStringsExtenisons.cs
@@ -2,31 +2,15 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.Framework.Configuration;
 
 namespace Microsoft.Framework.Configuration
 {
-    public static class ConnectionStringsExtenisons
+    public static partial class ConnectionStringsExtenisons
     {
-        /// <summary>
-        /// extracts connections strings from config.json into ConnectionStringSettingsCollection (eg. ConfigurationManager.ConnectionStrings)
-        /// </summary>
-        /// <param name="cfg"></param>
-        /// <param name="connectionStrings"> (eg. ConfigurationManager.ConnectionStrings)</param>
-        public static void ExtractConnectionStrings(this IConfiguration cfg, ConnectionStringSettingsCollection connectionStrings)
-        {
-           var section = cfg.GetSection("connectionStrings");
-            var subKeys = section.GetChildren();
-
-            foreach (var subkey in subKeys)
-            {
-                var name = subkey.Key;
-                var val = subkey.Value ?? subkey.Get("connectionString");
-                var provider = subkey.Get("provider") ?? "System.Data.SqlClient";
-                connectionStrings.AddConnectionString(name, val, provider);
-            }
-        }
+        
 
         public static string GetConnectionStringValue(this IConfiguration cfg, string name)
         {
@@ -38,6 +22,15 @@ namespace Microsoft.Framework.Configuration
             var val = subkey.Get("connectionString") ?? subkey.Value;
             return val;
             
+        }
+
+        public static string GetConnectionStringSecureValue(this IConfiguration cfg, string name)
+        {
+            var connStr = cfg.GetConnectionStringValue(name);
+            if (connStr == null) return null;
+            connStr = Regex.Replace(connStr, @"User Id\s*=.*?;", "User Id=***", RegexOptions.IgnoreCase);
+            connStr = Regex.Replace(connStr, @"Password\s*=.*?;", "User Id=***", RegexOptions.IgnoreCase);
+            return connStr;
         }
     }
 }
