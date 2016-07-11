e081f7a98fa9a399f0a570289fd0e3da5d496eb7
jakub.pawlowski <pawlowskijakub@gmail.com>
2016-07-11 08:04:26 +0200

    update initial boostrap

--- init.ps1
+++ init.ps1
@@ -1,1 +1,16 @@
-cmd /c mklink dnx-packages $env:userprofile\.nuget\packages /J
+pushd 
+
+cd $psscriptroot
+
+try {
+    if (!(test-path ".scripts")) { mkdir ".scripts" }
+    #init build tools
+	wget http://bit.ly/qbootstrap1 -UseBasicParsing -OutFile ".scripts/bootstrap.ps1" 
+    get-content ".scripts/bootstrap.ps1" | out-string | iex 
+    #prepare packages dir
+	if (!(test-path dnx-packages)) {
+		cmd /c mklink dnx-packages $env:userprofile\.nuget\packages /J
+	}
+} finally {
+	popd
+}
