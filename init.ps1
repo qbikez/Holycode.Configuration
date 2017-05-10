1833c023d8576c82852af910ce86666cd19eff63
Jakub Pawłowski <pawlowskijakub@gmail.com>
2017-05-10 17:13:55 +0200

    holycode.configuration: stable version 2.2.8\

--- init.ps1
+++ init.ps1
@@ -3,20 +3,28 @@ pushd
 cd $psscriptroot
 
 try {
+	write-progress "initializing .scripts dir"
     if (!(test-path ".scripts")) { mkdir ".scripts" }
     #init build tools
+    write-progress "initializing build tools using qbootstrap"
 	wget http://bit.ly/qbootstrap1 -UseBasicParsing -OutFile ".scripts/bootstrap.ps1" 
     get-content ".scripts/bootstrap.ps1" | out-string | iex
      
+     write-progress "preparing dnx-packages"
     # prepare packages dir
 	if (!(test-path dnx-packages)) {
 		cmd /c mklink dnx-packages $env:userprofile\.nuget\packages /J
 	}
+	
+	write-progress "installing required powershell modules"
 
     Install-Module pathutils
     refresh-env
 
     $dotnetver = "1.0.0-preview3-003171"
+    
+    write-progress "installing dotnet $dotnetver"
+    
     if ((get-command "dotnet" -ErrorAction Ignore) -eq $null -or ((dotnet --version) -ne $dotnetver)) {
         wget "https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1" -UseBasicParsing -OutFile ".scripts/dotnet-install.ps1" 
         ./.scripts/dotnet-install.ps1 -Version $dotnetver    
@@ -32,7 +40,8 @@ try {
 
     dotnet --info
 
-
+	write-progress "installing dnvm/dnu install"
+	
     wget "https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1" -UseBasicParsing -OutFile ".scripts/dnvm-install.ps1" 
     ./.scripts/dnvm-install.ps1
     dnvm install 1.0.0-rc1-update1 -alias default
